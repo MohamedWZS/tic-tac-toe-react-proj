49cefb3f0d64dfcfeb3fc388eb15124e989907b6
MohameWZS <mohamedwael.31.8.2000@gmail.com>
2024-06-24 16:27:30 +0300

    Updating Edit Button State Based on Old State Correctly

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
-    const [ isEditing, setIsEditing ] = useState(false);
-    
+export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
     function handleEditClick() {
-        setIsEditing(true);
+        // do not do that (suggested by react team)
+        // setIsEditing(!isEditing); // => schedules a state update to true
+        // setIsEditing(!isEditing); // => schedules also state update to true
+
+        // functional form of the state updater
+        // The function passed to setIsEditing receives the current state (isEditing) as prevIsEditing.
+        setIsEditing((prevIsEditing) => !prevIsEditing);
     }
 
     return (
         <li>
             <span className="player">
-                {
-                    isEditing?
-                    <input type="text" />
-                    :
+                {isEditing ? (
+                    <input type="text" required value={name} />
+                ) : (
                     <span className="player-name">{name}</span>
-                }
+                )}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>
+                {isEditing ? "Save" : "Edit"}
+            </button>
         </li>
     );
 }
