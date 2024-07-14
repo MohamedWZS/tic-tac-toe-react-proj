413617488bd8c4635518aac6a495c77e04af33ed
MohameWZS <mohamedwael.31.8.2000@gmail.com>
2024-07-14 16:55:54 +0300

    Updating Object State Immutably

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
-]
+    [null, null, null],
+];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            // you should not mutate objects & arrays directly - instead create a (deep) copy first!
+            const updatedBoard = [
+                ...prevGameBoard.map((innerArray) => [...innerArray]),
+            ];
+
+            updatedBoard[rowIndex][colIndex] = "X";
+
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) =>
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
-                        {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                            <button>{playerSymbol}</button>
-                        </li>)}
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
                     </ol>
                 </li>
-            )}
+            ))}
         </ol>
     );
 }
